4127e84d917ec203bce0765b75bd9e98681294c3
Richard Easton <richeaston@users.noreply.github.com>
2019-12-19 13:43:41 +0000

    Updated EdgeSearch.ps1
    Added kill tab to initial search tab to kill the tab after a period of secs, this is to stop Edge overloading the memory with too many tabs

--- Edgesearch.ps1
+++ Edgesearch.ps1
@@ -1,14 +1,15 @@
-﻿cls
+cls
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 
-for ($a = 0; $a -lt 40; $a++) {
+for ($a = 0; $a -lt 90; $a++) {
    $RandomWord = (48..57) + (65..90) + (97..122) | Get-Random -Count 1 | % {[char]$_}
    $RandomQuestion = Get-Random -InputObject("What+is+","Definition+of+","Pronunciation+of+","Thesaurus+","Examples+of+","prefixes+for+","suffixes+for+")
    Start-Process "microsoft-edge:http://www.bing.com/search?q=$RandomQuestion$RandomWord"  -WindowStyle Minimized
-   start-sleep -Milliseconds 800
+   start-sleep -Milliseconds 1000
+   $CP = Get-Process -Name MicrosoftEdgeCP | Select -ExpandProperty Processname
+   Stop-Process -Name MicrosoftEdgeCP -Force -PassThru
 }
-start-sleep -Milliseconds 2000
 
 $CP = Get-Process -Name MicrosoftEdgeCP | Select -ExpandProperty Processname
 $sh = Get-Process -Name MicrosoftEdgeSH | Select -ExpandProperty Processname
@@ -18,6 +19,7 @@ while ($cp.count -gt 0) {
     Start-Sleep -Milliseconds 200
     $CP = Get-Process -Name MicrosoftEdgeCP -ErrorAction SilentlyContinue | Select Processname
 }
+
 
 while ($sh.count -gt 0) {
     Stop-Process -Name MicrosoftEdgeSH -Force -PassThru
